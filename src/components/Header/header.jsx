2a7a9aa3a0e4862b84227c2159f3012fd1ec8e8a
Marek Sieńkowski <mareksienkowski.90@gmail.com>
2020-10-17 20:59:12 +0200

    new font

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import Logo from './Logo/logo';
+import HeaderStyled from './headerStyle'
 import Container from '../../settings/container';
 
 const Header = () => {
     return (
-        <>
+        <HeaderStyled>
             <Container>
                 <Logo/>
             </Container>
-        </>
+        </HeaderStyled>
     )
 }
 
--- src/components/Header/headerStyle.jsx
+++ src/components/Header/headerStyle.jsx
@@ -2,9 +2,10 @@ import styled from 'styled-components';
 
 const HeaderStyled = styled.header`
     width:100%;
-    /* height: 11.62vh; */
-    height:100%;
+    height: 11.733vh;
     transition: all 0.4s ease-in-out;
+    display: flex;
+    justify-content: center;
 `
 
 export default HeaderStyled;
